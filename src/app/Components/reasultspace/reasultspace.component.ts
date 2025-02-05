8cc5393fb414cdf115eb545db6e0c29e468adcef
alirafatartist <lraft636@gmail.com>
2025-02-05 11:32:52 +0200

    add animation and loading ... to bot message

--- src/app/Components/reasultspace/reasultspace.component.ts
+++ src/app/Components/reasultspace/reasultspace.component.ts
@@ -4,13 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import hljs from 'highlight.js';
-import 'highlight.js/styles/github-dark.css'; // or any other style you prefer
+import 'highlight.js/styles/github-dark.css';
 
 interface Message {
   type: 'user' | 'bot';
   content: string;
   isLoading?: boolean;
   htmlContent?: SafeHtml;
+  isTyping?: boolean;
+  displayContent?: string;
 }
 
 @Component({
@@ -29,8 +31,31 @@ export class ReasultspaceComponent implements OnInit, AfterViewChecked {
   isHidden: boolean = true;
   messages: Message[] = [];
 
+  typeMessage(message: Message, content: string) {
+    message.isTyping = true;
+    message.displayContent = '';
+    let index = 0;
+
+    const typeChar = () => {
+      if (index < content.length) {
+        message.displayContent += content[index];
+        index++;
+        setTimeout(typeChar, 10);
+      } else {
+        message.isTyping = false;
+        message.htmlContent = this.sanitizer.bypassSecurityTrustHtml(content);
+        setTimeout(() => {
+          document.querySelectorAll('pre code').forEach((block) => {
+            hljs.highlightElement(block as HTMLElement);
+          });
+        }, 0);
+      }
+    };
+
+    typeChar();
+  }
+
   ngOnInit() {
-    // Configure highlight.js
     hljs.configure({
       ignoreUnescapedHTML: true,
     });
@@ -38,34 +63,43 @@ export class ReasultspaceComponent implements OnInit, AfterViewChecked {
     this.SharedService.inputData$.subscribe((data) => {
       if (data) {
         this.isHidden = false;
+        // Add user message
         this.messages.push({
           type: 'user',
           content: data,
         });
-        this.messages.push({
+        // Add bot message with loading state
+        const botMessage: Message = {
           type: 'bot',
           content: 'Loading...',
           isLoading: true,
-        });
+          displayContent: 'Loading...',
+          htmlContent: this.sanitizer.bypassSecurityTrustHtml('Loading...'),
+        };
+        this.messages.push(botMessage);
       }
     });
 
     this.SharedService.botData$.subscribe((data) => {
       if (data) {
-        this.messages = this.messages.filter((msg) => !msg.isLoading);
-        this.messages.push({
-          type: 'bot',
-          content: data,
-          htmlContent: this.sanitizer.bypassSecurityTrustHtml(data),
-        });
+        // Find and update the loading message
+        const loadingMessage = this.messages.find((msg) => msg.isLoading);
+        if (loadingMessage) {
+          // Update the existing message with new content
+          loadingMessage.isLoading = false;
+          loadingMessage.content = data;
+          this.typeMessage(loadingMessage, data);
+        }
       }
     });
   }
 
   ngAfterViewChecked() {
-    // Find and highlight all code blocks
     document.querySelectorAll('pre code').forEach((block) => {
-      hljs.highlightElement(block as HTMLElement);
+      if (!block.classList.contains('highlighted')) {
+        hljs.highlightElement(block as HTMLElement);
+        block.classList.add('highlighted');
+      }
     });
   }
 }
